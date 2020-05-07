6f8e94095465092fb2907a2269f04884502b410b
tiennguyen <tien.nguyenv@enjoyworks.co.kr>
2020-05-07 16:57:12 +0700

    enroll org1 admin

---
+++ network-prod/scripts/enroll-org1-admin-peer1-org1.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+echo
+echo " ____    _____      _      ____    _____ "
+echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
+echo "\___ \    | |     / _ \   | |_) |   | |  "
+echo " ___) |   | |    / ___ \  |  _ <    | |  "
+echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
+echo
+echo "Define by Tien"
+echo
+cd $HOME
+
+echo " ------------------------------------------------------------------------------------------------------ "
+echo " Setup Org1's peer1"
+echo " ------------------------------------------------------------------------------------------------------ "
+
+echo " ------------------------------------------------------------------------------------------------------ "
+echo " This operation happening on peer1-org1"
+echo " ------------------------------------------------------------------------------------------------------ "
+echo " You must do configuration as the following before issuing this script"
+echo " - enable rca-org1 to be ssh with password"
+echo " - enable rca-org1 to be scp without password"
+
+set -x
+
+export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/org1/admin
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/org1/peer1/assets/ca/org1-ca-cert.pem
+export FABRIC_CA_CLIENT_MSPDIR=msp
+fabric-ca-client enroll -d -u https://admin-org1:org1AdminPW@rca-org1:7054
+
+mkdir /tmp/hyperledger/org1/peer1/msp/admincerts
+cp /tmp/hyperledger/org1/admin/msp/signcerts/cert.pem /tmp/hyperledger/org1/peer1/msp/admincerts/org1-admin-cert.pem
+
+set +x
+
+echo
+echo " _____   _   _   ____   "
+echo "| ____| | \ | | |  _ \  "
+echo "|  _|   |  \| | | | | | "
+echo "| |___  | |\  | | |_| | "
+echo "|_____| |_| \_| |____/  "
+echo
+
+echo " Completed Defining the network"
+exit 0
---
+++ network-prod/scripts/enroll-org1-admin-peer2-org1.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+echo
+echo " ____    _____      _      ____    _____ "
+echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
+echo "\___ \    | |     / _ \   | |_) |   | |  "
+echo " ___) |   | |    / ___ \  |  _ <    | |  "
+echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
+echo
+echo "Define by Tien"
+echo
+cd $HOME
+
+echo " ------------------------------------------------------------------------------------------------------ "
+echo " Setup Org1's peer1"
+echo " ------------------------------------------------------------------------------------------------------ "
+
+echo " ------------------------------------------------------------------------------------------------------ "
+echo " This operation happening on peer2-org1"
+echo " ------------------------------------------------------------------------------------------------------ "
+echo " You must do configuration as the following before issuing this script"
+echo " - enable peer1-org1 to be ssh with password"
+echo " - enable peer1-org1 to be scp without password"
+
+set -x
+
+mkdir -p /tmp/hyperledger/org1/peer2/msp/admincerts
+scp root@peer1-org1:/tmp/hyperledger/org1/admin/msp/signcerts/cert.pem /tmp/hyperledger/org1/peer2/msp/admincerts/org1-admin-cert.pem
+
+set +x
+
+echo
+echo " _____   _   _   ____   "
+echo "| ____| | \ | | |  _ \  "
+echo "|  _|   |  \| | | | | | "
+echo "| |___  | |\  | | |_| | "
+echo "|_____| |_| \_| |____/  "
+echo
+
+echo " Completed Defining the network"
+exit 0
