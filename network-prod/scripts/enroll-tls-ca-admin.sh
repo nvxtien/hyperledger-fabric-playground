fa57e11176a1c27c55496e79e86dbde9aa4e504b
tiennguyen <tien.nguyenv@enjoyworks.co.kr>
2020-05-07 14:03:39 +0700

    enroll tls ca admin

--- network-prod/scripts/enroll-tls-ca-admin.sh
+++ network-prod/scripts/enroll-tls-ca-admin.sh
@@ -26,7 +26,10 @@ scp root@tls-ca:/tmp/hyperledger/fabric-ca/crypto/ca-cert.pem /tmp/hyperledger/t
 export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/tls-ca/crypto/tls-ca-cert.pem
 export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/tls-ca/admin
 fabric-ca-client enroll -d -u https://tls-ca-admin:tls-ca-adminpw@tls-ca:7052
-fabric-ca-client register -d --id.name orderer1-org0 --id.secret ordererPW --id.type orderer -u https://tls-ca:7052
+
+#fabric-ca-client register -d --id.name orderer1-org0 --id.secret ordererPW --id.type orderer -u https://tls-ca:7052
+#fabric-ca-client register -d --id.name peer1-org1 --id.secret peer1PW --id.type peer -u https://tls-ca:7052
+#fabric-ca-client register -d --id.name peer2-org1 --id.secret peer2PW --id.type peer -u https://tls-ca:7052
 
 echo
 echo " _____   _   _   ____   "
