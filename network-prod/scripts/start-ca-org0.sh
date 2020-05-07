bda839b7a7205235ecb8108003da8e300524c142
tiennguyen <tien.nguyenv@enjoyworks.co.kr>
2020-05-07 12:00:04 +0700

    ca of org0

--- network-prod/scripts/start-ca-org0.sh
+++ network-prod/scripts/start-ca-org0.sh
@@ -15,10 +15,10 @@ echo " -------------------------------------------------------------------------
 curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.0.1 1.4.6 0.4.18
 
 echo " ------------------------------------------------------------------------------------------------------ "
-echo " Create TLS CA service "
+echo " Create CA service for Org0"
 echo " ------------------------------------------------------------------------------------------------------ "
 
-cat > tls-ca.service << EOF
+cat > rca-org0.service << EOF
 # Service definition for Hyperledger fabric-ca server
 [Unit]
 Description=hyperledger fabric-ca server - Certificate Authority for hyperledger fabric
@@ -31,16 +31,16 @@ Restart=on-failure
 Environment=FABRIC_CA_SERVER_DEBUG=true
 Environment=FABRIC_CA_SERVER_HOME=/tmp/hyperledger/fabric-ca/crypto
 Environment=FABRIC_CA_SERVER_TLS_ENABLED=true
-Environment=FABRIC_CA_SERVER_CSR_CN=tls-ca
-Environment=FABRIC_CA_SERVER_CSR_HOSTS=tls-ca
-ExecStart=/usr/local/bin/fabric-ca-server start -d -b tls-ca-admin:tls-ca-adminpw --port 7052
+Environment=FABRIC_CA_SERVER_CSR_CN=rca-org0
+Environment=FABRIC_CA_SERVER_CSR_HOSTS=rca-org0
+ExecStart=/usr/local/bin/fabric-ca-server start -d -b rca-org0-admin:rca-org0-adminpw --port 7053
 [Install]
 WantedBy=multi-user.target
 EOF
 
-sudo cp tls-ca.service /etc/systemd/system/
-sudo systemctl enable tls-ca.service
-sudo systemctl start tls-ca.service
+sudo cp rca-org0.service /etc/systemd/system/
+sudo systemctl enable rca-org0.service
+sudo systemctl start rca-org0.service
 sleep 5
 sudo systemctl status tls-ca.service
 
