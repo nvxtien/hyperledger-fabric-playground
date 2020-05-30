e66e89126632045e9dcdf0365033bd32b6bc780b
nvxtien <nvxtien@gmail.com>
2020-05-30 17:24:02 +0700

    tlsca.po1.fabric.com

--- network-prod/scripts/04-setup-peer-ca.sh
+++ network-prod/scripts/04-setup-peer-ca.sh
@@ -12,12 +12,12 @@ echo
 cd $HOME
 
 echo " ------------------------------------------------------------------------------------------------------ "
-echo " Setup peer CA "
+echo " Setup peer CA ca.po1.fabric.com"
 echo " ------------------------------------------------------------------------------------------------------ "
 
 set -x
-export FABRIC_CA_CLIENT_TLS_CERTFILES=/etc/hyperledger/fabric-ca/ca/ca.po1.fabric.com-cert.pem
-export FABRIC_CA_CLIENT_HOME=/etc/hyperledger/ca-admin
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/etc/hyperledger/po1.fabric.com/ca-server/ca/ca.po1.fabric.com-cert.pem
+export FABRIC_CA_CLIENT_HOME=/etc/hyperledger/po1.fabric.com/ca-admin
 
 fabric-ca-client enroll -d -u https://rca-po1-admin:rca-po1-adminpw@ca.po1.fabric.com:7153
 fabric-ca-client register -d --id.name peer0.po1.fabric.com --id.secret peer1PW --id.type peer -u https://ca.po1.fabric.com:7153
